0270cd5fc3d5c469169dc44917770060140df50f
Cloud User <centos@ip-172-31-20-77.eu-west-1.compute.internal>
2020-05-09 22:41:18 +0000

    push

--- gitlab_module.tf
+++ gitlab_module.tf
@@ -21,3 +21,27 @@ module "gitlab" {
     }
     private_key_path = "~/.ssh/id_rsa"  
 }
+
+module "gitlab" {
+    source = "./gitlab_project"
+    region = "us-east-2"
+    vpc_cidr = "192.168.0.0/16"
+    public_subnet_cidr = [
+        "192.168.1.0/24",
+        "192.168.2.0/24",
+        "192.168.3.0/24"
+]
+    private_subnet_cidr = [
+         "192.168.11.0/24",
+         "192.168.12.0/24",
+         "192.168.13.0/24"
+    ]
+
+    instance_type = "t2.xlarge"
+    key_name = "my_key"
+    public_key_path = "~/.ssh/id_rsa.pub"
+    tags = {
+     Environment = "Gitlab"
+    }
+    private_key_path = "~/.ssh/id_rsa"  
+}
